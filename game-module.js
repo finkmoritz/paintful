b79837391b9245c63ac4029b5abc1bcbe8276d32
moritz <fink.moritz@gmail.com>
2020-04-16 10:13:28 +0200

    Refactor game logic to module file

---
+++ game-module.js
@@ -0,0 +1,212 @@
+class Player {
+    constructor(socketId, username, color) {
+        this.socketId = socketId;
+        this.username = username;
+        this.color = color;
+        this.currentScreen = 'screenPlayers';
+        this.quest = '';
+        this.painting = undefined;
+        this.guess = undefined;
+        this.choice = undefined;
+        this.score = 0;
+        this.tendency = 0;
+        this.waitText = '';
+    }
+}
+
+class Game {
+    constructor(id) {
+        this.id = id;
+        this.players = [];
+        this.started = false;
+        this.finished = false;
+        this.currentRound = -1;
+        this.choices = [];
+    }
+}
+
+let maxGameId = 0;
+const games = new Map();
+const socketIdToGameKey = new Map();
+
+module.exports = {
+    getGame: function(socketId) {
+        let gameKey = socketIdToGameKey.get(socketId);
+        return games.get(gameKey);
+    },
+
+    addPlayer: function(game, socketId, username) {
+        const colors = ['blue', 'red', 'green', 'yellow'];
+        const nPlayers = game.players.length;
+        let newPlayer = new Player(socketId, username, colors[nPlayers]);
+        newPlayer.quest = this.generateQuest();
+        game.players.push(newPlayer);
+    },
+
+    generateQuest: function() {
+        const adjectives = ['Angry', 'Frightened', 'Lazy', 'Burning'];
+        const nominals = ['Unicorn', 'Mailman', 'Bird', 'Pizza'];
+        let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+        let randomNominal = nominals[Math.floor(Math.random() * nominals.length)];
+        return randomAdjective+' '+randomNominal;
+    },
+
+    getPlayer: function(socketId) {
+        const game = this.getGame(socketId);
+        for(let player of game.players) {
+            if(player.socketId === socketId) {
+                return player;
+            }
+        }
+        return undefined;
+    },
+
+    newGame: function(socketId, username) {
+        const game = new Game(++maxGameId);
+        games.set(maxGameId, game);
+        this.addPlayer(game, socketId, username);
+        socketIdToGameKey.set(socketId, maxGameId);
+        return game;
+    },
+
+    joinGame: function(socketId, username, gameId) {
+        socketIdToGameKey.set(socketId, gameId);
+        const game = this.getGame(socketId);
+        this.addPlayer(game, socketId, username);
+        return game;
+    },
+
+    startGame: function(socketId) {
+        const game = this.getGame(socketId);
+        game.started = true;
+        for(let p of game.players) {
+            p.currentScreen = 'screenDraw';
+        }
+        return game;
+    },
+
+    setPainting: function(socketId, painting) {
+        const player = this.getPlayer(socketId);
+        player.painting = painting;
+        player.currentScreen = 'screenWait';
+        player.waitText = 'Waiting for the other players to finish their painting...';
+    },
+
+    allPaintingsSubmitted: function(game) {
+        for(let p of game.players) {
+            if(p.painting === undefined) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    nextRound: function(game) {
+        game.currentRound += 1;
+        for(let p of game.players) {
+            p.currentScreen = 'screenGuess';
+            p.guess = undefined;
+            p.choice = undefined;
+            p.tendency = 0;
+        }
+        game.players[game.currentRound].currentScreen = 'screenWait';
+        game.players[game.currentRound].waitText = 'Waiting until the other players have made their guess on your painting...';
+        return game;
+    },
+
+    setGuess: function(socketId, guess) {
+        const player = this.getPlayer(socketId);
+        player.guess = guess;
+        player.currentScreen = 'screenWait';
+        player.waitText = 'Waiting for the other players to submit their guess...';
+    },
+
+    allGuessesSubmitted: function(game) {
+        let currentPlayer = game.players[game.currentRound];
+        for(let p of game.players) {
+            if(p.socketId !== currentPlayer.socketId && p.guess === undefined) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    showChoices: function(game) {
+        game.choices = [];
+        game.choices.push(game.players[game.currentRound].quest);
+        for(let p of game.players) {
+            if(p.guess !== undefined) {
+                game.choices.push(p.guess);
+            }
+        }
+        for(let p of game.players) {
+            p.currentScreen = 'screenChoices';
+        }
+        game.players[game.currentRound].currentScreen = 'screenWait';
+        game.players[game.currentRound].waitText = 'Waiting until the other players have made their guess on your painting...';
+        return game;
+    },
+
+    setChoice: function(socketId, choice) {
+        const player = this.getPlayer(socketId);
+        player.choice = choice;
+        player.currentScreen = 'screenWait';
+        player.waitText = 'Waiting for the other players to submit their choice...';
+    },
+
+    allChoicesSubmitted: function(game) {
+        let currentPlayer = game.players[game.currentRound];
+        for(let p of game.players) {
+            if(p.socketId !== currentPlayer.socketId && p.choice === undefined) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    evaluateRound: function(game) {
+        const correctAnswer = game.players[game.currentRound].quest;
+        for(let p of game.players) {
+            if(p.choice === undefined) continue;
+            if(p.guess === correctAnswer) {
+                p.score += 5;
+                p.tendency += 5;
+            }
+            if(p.choice === correctAnswer) {
+                p.score += 2;
+                p.tendency += 2;
+            } else {
+                for(let otherPlayer of game.players) {
+                    if(p.socketId === otherPlayer.socketId) continue;
+                    if(p.choice === otherPlayer.guess) {
+                        otherPlayer.score += 1;
+                        otherPlayer.tendency += 1;
+                        //TODO break; when choices displayed are unique
+                    }
+                }
+            }
+        }
+        for(let p of game.players) {
+            p.currentScreen = 'screenResults';
+        }
+        if(game.currentRound === game.players.length-1) {
+            game.finished = true;
+        }
+        return game;
+    },
+
+    handleError: function(e, socket) {
+        console.error('ERROR receiving message from socket with ID '+socket.id);
+        console.error('Original error: '+e.stack);
+        this.logMap(socketIdToGameKey);
+        this.logMap(games);
+        io.to(socket.id).emit('error', 'Oops, looks like something went wrong...');
+        console.error('Rooms: '+JSON.stringify(socket.rooms));
+    },
+
+    logMap: function(map) {
+        for(let k of map.keys()) {
+            console.log(k+' -> '+JSON.stringify(map.get(k)));
+        }
+    }
+};
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const server = http.createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
 const SocketIOFileUpload = require("socketio-file-upload");
+const GameModule = require('./game-module');
+
+const uploadDir = "/tmp";
+
 SocketIOFileUpload.listen(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,10 +45,10 @@ io.on('connection', function(socket){
     uploader.on("saved", function (e) {
         try {
             console.log('Upload saved: '+JSON.stringify(e.file));
-            setPainting(socket.id, e.file.name);
-            let game = getGame(socket.id);
-            if(allPaintingsSubmitted(game)) {
-                game = nextRound(game);
+            GameModule.setPainting(socket.id, e.file.name);
+            let game = GameModule.getGame(socket.id);
+            if(GameModule.allPaintingsSubmitted(game)) {
+                game = GameModule.nextRound(game);
                 console.log('emit to '+game.id+': update game '+JSON.stringify(game));
                 io.in(game.id).emit('update game', game);
             } else {
@@ -52,19 +56,19 @@ io.on('connection', function(socket){
                 io.to(socket.id).emit('update game', game);
             }
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
     socket.on('new game', function(username) {
         try {
             console.log('new game '+username);
-            const game = newGame(socket.id, username);
+            const game = GameModule.newGame(socket.id, username);
             socket.join(game.id);
             console.log('emit to '+game.id+': update game '+JSON.stringify(game));
             io.in(game.id).emit('update game', game);
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
@@ -72,33 +76,33 @@ io.on('connection', function(socket){
         try {
             let gameId = parseInt(data.gameId);
             console.log('join game '+data.username+', '+gameId);
-            const game = joinGame(socket.id, data.username, gameId);
+            const game = GameModule.joinGame(socket.id, data.username, gameId);
             socket.join(game.id);
             console.log('emit to '+game.id+': update game '+JSON.stringify(game));
             io.in(game.id).emit('update game', game);
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
     socket.on('start game', function () {
         try {
             console.log('start game');
-            const game = startGame(socket.id);
+            const game = GameModule.startGame(socket.id);
             console.log('emit to '+game.id+': update game '+JSON.stringify(game));
             io.in(game.id).emit('update game', game);
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
     socket.on('guess', function (guess) {
         try {
             console.log('guess: '+guess);
-            setGuess(socket.id, guess);
-            let game = getGame(socket.id);
-            if(allGuessesSubmitted(game)) {
-                game = showChoices(game);
+            GameModule.setGuess(socket.id, guess);
+            let game = GameModule.getGame(socket.id);
+            if(GameModule.allGuessesSubmitted(game)) {
+                game = GameModule.showChoices(game);
                 console.log('emit to '+game.id+': update game '+JSON.stringify(game));
                 io.in(game.id).emit('update game', game);
             } else {
@@ -106,17 +110,17 @@ io.on('connection', function(socket){
                 io.to(socket.id).emit('update game', game);
             }
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
     socket.on('choice', function (choice) {
         try {
             console.log('choice: '+choice);
-            setChoice(socket.id, choice);
-            let game = getGame(socket.id);
-            if(allChoicesSubmitted(game)) {
-                game = evaluateRound(game);
+            GameModule.setChoice(socket.id, choice);
+            let game = GameModule.getGame(socket.id);
+            if(GameModule.allChoicesSubmitted(game)) {
+                game = GameModule.evaluateRound(game);
                 console.log('emit to '+game.id+': update game '+JSON.stringify(game));
                 io.in(game.id).emit('update game', game);
             } else {
@@ -124,19 +128,19 @@ io.on('connection', function(socket){
                 io.to(socket.id).emit('update game', game);
             }
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
     socket.on('next round', function () {
         try {
             console.log('next round');
-            let game = getGame(socket.id);
-            game = nextRound(game);
+            let game = GameModule.getGame(socket.id);
+            game = GameModule.nextRound(game);
             console.log('emit to '+game.id+': update game '+JSON.stringify(game));
             io.in(game.id).emit('update game', game);
         } catch(e) {
-            handleError(e, socket);
+            GameModule.handleError(e, socket);
         }
     });
 
@@ -148,215 +152,3 @@ io.on('connection', function(socket){
 server.listen(3000, function(){
     console.log('listening on *:3000');
 });
-
-class Player {
-    constructor(socketId, username, color) {
-        this.socketId = socketId;
-        this.username = username;
-        this.color = color;
-        this.currentScreen = 'screenPlayers';
-        this.quest = '';
-        this.painting = undefined;
-        this.guess = undefined;
-        this.choice = undefined;
-        this.score = 0;
-        this.tendency = 0;
-        this.waitText = '';
-    }
-}
-
-class Game {
-    constructor(id) {
-        this.id = id;
-        this.players = [];
-        this.started = false;
-        this.finished = false;
-        this.currentRound = -1;
-        this.choices = [];
-    }
-}
-
-let maxGameId = 0;
-const games = new Map();
-const socketIdToGameKey = new Map();
-const uploadDir = "/tmp";
-
-function getGame(socketId) {
-    let gameKey = socketIdToGameKey.get(socketId);
-    return games.get(gameKey);
-}
-
-function addPlayer(game, socketId, username) {
-    const colors = ['blue', 'red', 'green', 'yellow'];
-    const nPlayers = game.players.length;
-    let newPlayer = new Player(socketId, username, colors[nPlayers]);
-    newPlayer.quest = generateQuest();
-    game.players.push(newPlayer);
-}
-
-function generateQuest() {
-    const adjectives = ['Angry', 'Frightened', 'Lazy', 'Burning'];
-    const nominals = ['Unicorn', 'Mailman', 'Bird', 'Pizza'];
-    let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    let randomNominal = nominals[Math.floor(Math.random() * nominals.length)];
-    return randomAdjective+' '+randomNominal;
-}
-
-function getPlayer(socketId) {
-    const game = getGame(socketId);
-    for(let player of game.players) {
-        if(player.socketId === socketId) {
-            return player;
-        }
-    }
-    return undefined;
-}
-
-function newGame(socketId, username) {
-    const game = new Game(++maxGameId);
-    games.set(maxGameId, game);
-    addPlayer(game, socketId, username);
-    socketIdToGameKey.set(socketId, maxGameId);
-    return game;
-}
-
-function joinGame(socketId, username, gameId) {
-    socketIdToGameKey.set(socketId, gameId);
-    const game = getGame(socketId);
-    addPlayer(game, socketId, username);
-    return game;
-}
-
-function startGame(socketId) {
-    const game = getGame(socketId);
-    game.started = true;
-    for(let p of game.players) {
-        p.currentScreen = 'screenDraw';
-    }
-    return game;
-}
-
-function setPainting(socketId, painting) {
-    const player = getPlayer(socketId);
-    player.painting = painting;
-    player.currentScreen = 'screenWait';
-    player.waitText = 'Waiting for the other players to finish their painting...';
-}
-
-function allPaintingsSubmitted(game) {
-    for(let p of game.players) {
-        if(p.painting === undefined) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function nextRound(game) {
-    game.currentRound += 1;
-    for(let p of game.players) {
-        p.currentScreen = 'screenGuess';
-        p.guess = undefined;
-        p.choice = undefined;
-        p.tendency = 0;
-    }
-    game.players[game.currentRound].currentScreen = 'screenWait';
-    game.players[game.currentRound].waitText = 'Waiting until the other players have made their guess on your painting...';
-    return game;
-}
-
-function setGuess(socketId, guess) {
-    const player = getPlayer(socketId);
-    player.guess = guess;
-    player.currentScreen = 'screenWait';
-    player.waitText = 'Waiting for the other players to submit their guess...';
-}
-
-function allGuessesSubmitted(game) {
-    let currentPlayer = game.players[game.currentRound];
-    for(let p of game.players) {
-        if(p.socketId !== currentPlayer.socketId && p.guess === undefined) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function showChoices(game) {
-    game.choices = [];
-    game.choices.push(game.players[game.currentRound].quest);
-    for(let p of game.players) {
-        if(p.guess !== undefined) {
-            game.choices.push(p.guess);
-        }
-    }
-    for(let p of game.players) {
-        p.currentScreen = 'screenChoices';
-    }
-    game.players[game.currentRound].currentScreen = 'screenWait';
-    game.players[game.currentRound].waitText = 'Waiting until the other players have made their guess on your painting...';
-    return game;
-}
-
-function setChoice(socketId, choice) {
-    const player = getPlayer(socketId);
-    player.choice = choice;
-    player.currentScreen = 'screenWait';
-    player.waitText = 'Waiting for the other players to submit their choice...';
-}
-
-function allChoicesSubmitted(game) {
-    let currentPlayer = game.players[game.currentRound];
-    for(let p of game.players) {
-        if(p.socketId !== currentPlayer.socketId && p.choice === undefined) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function evaluateRound(game) {
-    const correctAnswer = game.players[game.currentRound].quest;
-    for(let p of game.players) {
-        if(p.choice === undefined) continue;
-        if(p.guess === correctAnswer) {
-            p.score += 5;
-            p.tendency += 5;
-        }
-        if(p.choice === correctAnswer) {
-            p.score += 2;
-            p.tendency += 2;
-        } else {
-            for(let otherPlayer of game.players) {
-                if(p.socketId === otherPlayer.socketId) continue;
-                if(p.choice === otherPlayer.guess) {
-                    otherPlayer.score += 1;
-                    otherPlayer.tendency += 1;
-                    //TODO break; when choices displayed are unique
-                }
-            }
-        }
-    }
-    for(let p of game.players) {
-        p.currentScreen = 'screenResults';
-    }
-    if(game.currentRound === game.players.length-1) {
-        game.finished = true;
-    }
-    return game;
-}
-
-function handleError(e, socket) {
-    console.error('ERROR receiving message from socket with ID '+socket.id);
-    console.error('Original error: '+e.stack);
-    logMap(socketIdToGameKey);
-    logMap(games);
-    io.to(socket.id).emit('error', 'Oops, looks like something went wrong...');
-    console.error('Rooms: '+JSON.stringify(socket.rooms));
-}
-
-function logMap(map) {
-    for(let k of map.keys()) {
-        console.log(k+' -> '+JSON.stringify(map.get(k)));
-    }
-}
